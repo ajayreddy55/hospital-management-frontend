26d4e5f34224179988416d5c71faace00af34344
ajayreddy55 <ajayreddy6753@gmail.com>
2023-11-03 18:23:18 +0530

    navbar commit

---
+++ src/components/bayannohome/index.js
@@ -0,0 +1,12 @@
+import Navbar from "../navbar";
+import "./index.css";
+
+const BayannoHome = () => {
+  return (
+    <div className="bayanno-home-bg-container">
+      <Navbar />
+    </div>
+  );
+};
+
+export default BayannoHome;
---
+++ src/components/navbar/index.js
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+import Popup from "reactjs-popup";
+import bayannoHospitalLogo from "../../assets/bayanno-hospital-logo.png";
+
+import "./index.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg  navbar-light bg-light navbar-bg-color">
+      <Link className="navbar-brand">
+        <div className="navbar-logo-container">
+          <img
+            src={bayannoHospitalLogo}
+            alt="logo"
+            className="navbar-logo-image"
+          />
+          <p className="navbar-logo-name">Bayanno Diagnostic Center</p>
+        </div>
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon toggler-icon-navbar"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav ml-auto">
+          <Link className="nav-link active navbar-link-item">HOME</Link>
+          <div className="nav-link">
+            <Popup
+              trigger={
+                <button type="button" className="navbar-departments-button">
+                  DEPARTMENTS
+                </button>
+              }
+              position={"bottom center"}
+              on={["hover", "click"]}
+              arrow={true}
+              className="popup-content"
+            >
+              <div className="navbar-tooltip-dep-container">
+                <Link className="navbar-dep-link-item">Anesthetics</Link>
+                <Link className="navbar-dep-link-item mt-2">Cardiology</Link>
+                <Link className="navbar-dep-link-item mt-2">
+                  Gastroenterology
+                </Link>
+              </div>
+            </Popup>
+          </div>
+          <Link className="nav-link navbar-link-item">DOCTORS</Link>
+          <Link className="nav-link navbar-link-item">ABOUT</Link>
+          <Link className="nav-link navbar-link-item">APPOINTMENT</Link>
+          <Link className="nav-link navbar-link-item">BLOG</Link>
+          <Link className="nav-link navbar-link-item">CONTACT</Link>
+          <Link className="nav-link navbar-link-item">LOGIN</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
